75e4e09e9e2a9e4fda8242fadcc595af5fb9dd6c
mmhh2024 <hayett.makhlouf@gmail.com>
2024-03-20 15:41:38 +0100

    Auto stash before merge of "main" and "origin/main"
    resolution conflit main

--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -1,12 +1,15 @@
 package nintendo.test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import nintendo.model.Achat;
+import nintendo.model.Adresse;
 import nintendo.model.Boutique;
 import nintendo.model.Client;
-
+import nintendo.model.Jeu;
+import nintendo.model.Portable;
 
 
 public class Test {
@@ -14,7 +17,18 @@ public class Test {
 	public static void main(String[] args) {
 		Client c1=new Client("SAMY", "MA");
 		Client c2=new Client("DUPOND", "JO");
+		Adresse adresse = new Adresse(12, "rue des Tulipes", "Paris"); 
+		Boutique b1 = new Boutique("Magic", adresse); 
+				 
+ 
+		 Portable p1 = new Portable("GameBoy",150.0,LocalDate.now()); 
+		 Jeu j1 = new Jeu("Mario",p1,b1); 
+		 Jeu j2 = new Jeu("Foot",p1,b1); 
+ 
+ 
+ 
 		
+
 		List<Achat> achat=new ArrayList();
 		
 
